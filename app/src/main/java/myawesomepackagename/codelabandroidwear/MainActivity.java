2bfd7ec7d12e319511ba6741e2539ecff361bd54
Marcus Gabilheri <marcusandreog@gmail.com>
2014-06-23 01:28:32 -0500

    First commit

---
+++ app/src/main/java/myawesomepackagename/codelabandroidwear/MainActivity.java
@@ -0,0 +1,192 @@
+package myawesomepackagename.codelabandroidwear;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.preview.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.NotificationCompat;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+/**
+ * The MIT License (MIT)
+
+ Copyright (c) <year> <copyright holders>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+/**
+ * @author Marcus Gabilheri
+ * @version 1.0
+ * @since June 23rd, 2014
+ *
+ * This codelab was developed to the GDG OSU Stillwater.
+ * Anyone can use this or modify for it's own purpose
+ */
+public class MainActivity extends Activity {
+
+    private EditText mCustomTitle, mCustomMessage;
+    private RadioGroup mCustomIconGroup, showHideIconGroup;
+    private int mCustomIcon;
+    private boolean showIcon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCustomTitle = (EditText) findViewById(R.id.notificationTitle);
+        mCustomMessage = (EditText) findViewById(R.id.notificationMessage);
+        mCustomIconGroup = (RadioGroup) findViewById(R.id.iconGroup);
+
+        mCustomIconGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (group.getCheckedRadioButtonId()) {
+                    case R.id.icon1:
+                        mCustomIcon = R.drawable.ic_wear_notification;
+                        break;
+                    case R.id.icon2:
+                        mCustomIcon = R.drawable.ic_notification_2;
+                        break;
+                    case R.id.icon3:
+                        mCustomIcon = R.drawable.ic_notification3;
+                        break;
+                }
+            }
+        });
+
+        showHideIconGroup = (RadioGroup) findViewById(R.id.hideIconGroup);
+        showHideIconGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (group.getCheckedRadioButtonId()) {
+                    case R.id.showIcon:
+                        showIcon = true;
+                        break;
+                    case R.id.hideIcon:
+                        showIcon = false;
+                        break;
+                }
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Define the method to send the notifications with the same name from the Android onClick from the XML Layout
+    public void sendNotification(View view) {
+
+        String eventTitle = "Sample Notification";
+        String eventText = "Text for the notification.";
+        String intentExtra = "This is an extra String!";
+        String eventDescription = "This is supposed to  be a content that will not fit the normal content screen"
+                + " usually a bigger text, by example a long text message or email.";
+
+        int notificationId = 001;
+        // Build intent for notification content
+        Intent viewIntent = new Intent(this, MainActivity.class);
+        PendingIntent viewPendingIntent =
+                PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        // Specify the 'big view' content to display the long
+        // event description that may not fit the normal content text.
+        NotificationCompat.BigTextStyle bigStyle = new NotificationCompat.BigTextStyle();
+
+
+        NotificationCompat.Builder mBuilder = null;
+
+        switch (view.getId()) {
+            case R.id.simpleNotification:
+                mBuilder = new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_wear_notification)
+                        .setContentTitle(eventTitle)
+                        .setContentText(eventText)
+                        .setContentIntent(viewPendingIntent);
+                break;
+            case R.id.bigNotification:
+                bigStyle.bigText(eventDescription); // bigText will override setContentText
+                bigStyle.setBigContentTitle("Override Title"); // bigContentTitle Override the contentTitle
+                mBuilder = new NotificationCompat.Builder(this)
+                    .setSmallIcon(R.drawable.ic_wear_notification)
+                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_sample_codelab))
+                    .setContentTitle(eventTitle) // This is unnecessary for the big notification if you use bigText
+                    .setContentText(eventText) // Unnecessary if setBigContentTitle is Overriden
+                    .setContentIntent(viewPendingIntent)
+                    .setStyle(bigStyle);
+                break;
+            case R.id.bigNotificationWithAction:
+
+                Intent photoIntent = new Intent(this, SecondActivity.class); // Intent pointing to our second activity
+                photoIntent.putExtra("message", intentExtra); // Set the extra message that will open in the next activity
+                photoIntent.putExtra("photo", R.drawable.ic_sample_codelab); // Send the photo to the next activity
+
+                PendingIntent photoPending = PendingIntent.getActivity(this, 0, photoIntent, 0); // set a new pending intent
+
+                bigStyle.bigText("Check out this picture!! :D");
+                mBuilder = new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_wear_notification)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_sample_codelab))
+                        .setContentIntent(viewPendingIntent) // This will be the default OPEN button.
+                        .addAction(R.drawable.ic_photo, "See Photo", photoPending) // This is our extra action. With an Extra Icon and pointing to the other PendingIntent
+                        .setStyle(bigStyle);
+                break;
+            case R.id.sendCustomNotification:
+
+                mBuilder = new NotificationCompat.Builder(this)
+                        .setSmallIcon(mCustomIcon)
+                        .setContentTitle(mCustomTitle.getText().toString())
+                        .setContentText(mCustomMessage.getText().toString())
+                        .setContentIntent(viewPendingIntent);
+                break;
+
+        }
+
+        // Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+        // Build the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, mBuilder.build());
+    }
+}
---
+++ app/src/main/java/myawesomepackagename/codelabandroidwear/SecondActivity.java
@@ -0,0 +1,78 @@
+package myawesomepackagename.codelabandroidwear;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * The MIT License (MIT)
+
+ Copyright (c) <year> <copyright holders>
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
+
+/**
+ * @author Marcus Gabilheri
+ * @version 1.0
+ * @since June 23rd, 2014
+ *
+ * This codelab was developed to the GDG OSU Stillwater.
+ * Anyone can use this or modify for it's own purpose
+ */
+public class SecondActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        Intent extraIntent = getIntent();
+
+        TextView mTextView = (TextView) findViewById(R.id.extraMessage);
+        ImageView mImageView = (ImageView) findViewById(R.id.extraPhoto);
+
+        mTextView.setText(extraIntent.getStringExtra("message")); // Retrieve the text and set it to our TextView
+        mImageView.setImageResource(extraIntent.getIntExtra("photo", 0)); // The zero is a default value in case the intent extra is empty.
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.second, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
